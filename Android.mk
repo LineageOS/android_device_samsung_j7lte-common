5e4efd0975b4143be17ef0608364551b10ed948a
Zvikomborero Vincent Zvikaramba <zvikovincent@gmail.com>
2017-08-29 21:18:23 -0400

    j7lte-common: Remove RemovePackages
     * This never worked at all.
    
    Change-Id: Ibe994c2725e803e2ef871375c9e1eae19ee9b736

--- Android.mk
+++ Android.mk
@@ -19,19 +19,4 @@ ifneq ($(filter j7ltespr j7ltechn, $(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := RemovePackages
-LOCAL_MODULE_TAGS := optional
-LOCAL_CERTIFICATE := shared
-LOCAL_MODULE_CLASS := FAKE
-# It overrrides packages to avoid their addition to the ROM
-LOCAL_OVERRIDES_PACKAGES := \
-	libsecril-client \
-	librilutils \
-	libreference-ril \
-	libril \
-	rild
-
-include $(BUILD_PHONY_PACKAGE)
-
 endif
