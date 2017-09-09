bb8cec9995044b3c514d646708d5ece64bc8f887
Emery Tanghanwaye <emerytang@gmail.com>
2017-09-09 02:44:01 -0400

    product: system_prop: bump volume steps
    Change-Id: Ic6d4eb7cd740c7c0ee730463d609aa5e919ae974

--- product/system_prop.mk
+++ product/system_prop.mk
@@ -9,8 +9,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Volume
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.config.vc_call_vol_steps=7 \
-	ro.config.media_vol_steps=14
+	ro.config.vc_call_vol_steps=15 \
+	ro.config.media_vol_steps=20
 
 # Chipname
 PRODUCT_PROPERTY_OVERRIDES += \
